cb701068890592ea1183c8d5628188953d0ade36
ped90 <ped90@ped90-GE62VR-7RF>
2017-05-29 11:12:27 +0200

    Introducion de atributos en la clase parametros con sus parametros constructores,get,set y tostring

--- src/basescd/parametros.java
+++ src/basescd/parametros.java
@@ -11,4 +11,38 @@ package basescd;
  */
 public class parametros {
     
+    String nombre;
+    String dni;
+
+    public parametros() {
+    }
+    
+    public parametros(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return "parametros{" + "nombre=" + nombre + ", dni=" + dni + '}';
+    }
+    
+    
+    
 }
