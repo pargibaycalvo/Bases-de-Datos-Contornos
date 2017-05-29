1196dc69ef4a6d5c91494788b3334f91f5142ca6
ped90 <ped90@ped90-GE62VR-7RF>
2017-05-29 11:14:39 +0200

    Creacion de los metodos necesarios para conectarnos al archivo de bases de datos indicandole la url

--- src/basescd/conecta.java
+++ src/basescd/conecta.java
@@ -5,10 +5,42 @@
  */
 package basescd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ped90
  */
 public class conecta {
+    
+    private String url;
+
+    public conecta() {
+        url="default.db";
+    }
+
+    public conecta(String url) {
+        this.url = url;
+    }
+    
+    public void Conexion(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+        }
+        catch (ClassNotFoundException e) {
+        JOptionPane.showMessageDialog(null, e.getMessage());
+        }  
+        try {
+         Connection conexion = DriverManager.getConnection("jdbc:sqlite:"+url);
+         System.out.println("Esto funciona madafakers");
+        }
+        catch (SQLException e) {
+         System.out.println("Buuuu");
+        JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
     
 }
